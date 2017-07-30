528caaa6a46d3642f272e3d1c238fa718c403094
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-07-30 13:49:01 -0400

    implement materialize via cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,6 +75,8 @@
     </div>
 
     <!-- Scripts -->
+    <!-- Materialize -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/js/materialize.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
