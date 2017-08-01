469e4f005530e6c8d4460fd48177f4396be68655
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-08-01 18:17:06 -0400

    add adventurer icon to map

--- resources/assets/js/components/MapDashboard.vue
+++ resources/assets/js/components/MapDashboard.vue
@@ -6,6 +6,7 @@
 
 <script>
 import mapStyles from './mapjs/map-styles';
+import {generateAdventurer, adventurerIcon} from './mapjs/adventurer.js';
 
     export default {
         data() {
@@ -13,26 +14,61 @@ import mapStyles from './mapjs/map-styles';
                 map: null,
                 infoWindow: null,
                 pos: null,
+                adventurerMarker: null,
             }
-        },
+        }, //end data
 
         methods: {
             initMap() {
                 this.map = new google.maps.Map($('#map')[0], {
                   center: {lat: 38.0423268, lng: -84.49276569999999},
                   zoom: 14,
-                  styles: mapStyles['dark']
+                  styles: mapStyles['army']
                 });
                 this.infoWindow = new google.maps.InfoWindow;
 
-                // this.getCurrentLocation();
+                this.getCurrentLocation();
             },
-        },
+
+            getCurrentLocation() {
+                // Try HTML5 geolocation.
+              if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                  this.pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                  };
+
+                  this.infoWindow.setPosition(this.pos);
+                  this.infoWindow.setContent('Adventurer, you are here');
+                  this.infoWindow.open(this.map);
+                  this.map.setCenter(this.pos);
+                  //Generates Adventurer Marker
+                  generateAdventurer(this.pos, this.map, this.adventurerMarker);
+
+                }, () => {
+                  this.handleLocationError(true, this.infoWindow, this.map.getCenter());
+                });
+              } else {
+                // Browser doesn't support Geolocation
+                this.handleLocationError(false, this.infoWindow, this.map.getCenter());
+              }
+            },
+
+            handleLocationError(browserHasGeolocation, infoWindow, pos) {
+                infoWindow.setPosition(pos);
+                infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+                infoWindow.open(this.map);
+            },
+        }, //end methods
 
         mounted() {
-            console.log($('#map')[0]);
+            console.log(generateAdventurer);
+            console.log(adventurerIcon);
             this.initMap();
-        }
+        }, //end mounted
     }
 </script>
 
@@ -41,3 +77,4 @@ import mapStyles from './mapjs/map-styles';
           height: 80vh;
       }
 </style>
+
