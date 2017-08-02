65d30913ec0350469e1bb3b37cb85ab899806773
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-08-02 10:22:30 -0400

    condensed map dashboard methods into import

--- resources/assets/js/components/MapDashboard.vue
+++ resources/assets/js/components/MapDashboard.vue
@@ -5,8 +5,8 @@
 </template>
 
 <script>
-import mapStyles from './mapjs/map-styles';
 import mapIcon from './mapjs/map-icons'; //import icons for map objects
+import adventurerMethods from './mapjs/adventurer';
 
     export default {
         data() {
@@ -28,136 +28,7 @@ import mapIcon from './mapjs/map-icons'; //import icons for map objects
             }
         }, //end data
 
-        methods: {
-            initMap() {
-                this.map = new google.maps.Map($('#map')[0], {
-                  center: {lat: 38.0423268, lng: -84.49276569999999},
-                  zoom: 14,
-                  styles: mapStyles['army']
-                });
-                this.infoWindow = new google.maps.InfoWindow;
-
-                this.getCurrentLocation();
-                this.updateAdventurerPosition(this.infoWindow, this.map, this.handleLocationError);
-            },
-
-            getCurrentLocation() {
-                // Try HTML5 geolocation.
-              if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition((position) => {
-
-                  this.pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                  };
-
-                  this.infoWindow.setPosition(this.pos);
-                  this.infoWindow.setContent('Adventurer, you are here');
-                  this.infoWindow.open(this.map);
-
-                  this.map.setCenter(this.pos);
-
-                  //Generates Adventurer Marker
-                  this.generateAdventurer(this.pos, this.map);
-
-                }, () => {
-                  this.handleLocationError(true, this.infoWindow, this.map.getCenter());
-                });
-              } else {
-                // Browser doesn't support Geolocation
-                this.handleLocationError(false, this.infoWindow, this.map.getCenter());
-              }
-            }, //end getCurrentLocation
-
-            handleLocationError(browserHasGeolocation, infoWindow, pos) {
-                infoWindow.setPosition(pos);
-                infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-                infoWindow.open(this.map);
-            }, //end handleLocationError
-
-            generateAdventurer () {
-                this.adventurerMarker = new google.maps.Marker({
-                      position: this.pos,
-                      map: this.map,
-                      icon: this.adventurerIcon,
-                      title: 'Adventurer, you are here'
-                    });
-
-                this.showHideEncounterRange();
-            }, //end generateAdventurer
-
-            updateAdventurerPosition (infoWindow, map, handleLocationError) {
-                // Try HTML5 geolocation.
-                if (navigator.geolocation) {
-
-                    //Clear old watch first, then create new one
-                    navigator.geolocation.clearWatch(this.watchId);
-
-                    //Set watch id and watch position
-                     this.watchId = navigator.geolocation.watchPosition(this.updateAdventurerSuccess, () => {
-                          this.handleLocationError(true, infoWindow, map.getCenter());
-                          }, { enableHighAccuracy: true, timeout: (10 * 1000 * 1000), maximumAge: (10 * 1000) }
-                          );
-
-                } else {
-                    // Browser doesn't support Geolocation
-                    this.handleLocationError(false, infoWindow, map.getCenter());
-                }
-            }, //end updateAdventurerPosition
-
-            pauseUpdateAdventurerPosition () {
-                if (navigator.geolocation) {
-                    navigator.geolocation.clearWatch(this.watchId);
-                }
-            }, //end updateAdventurerPositionStop
-
-            updateAdventurerSuccess (position) {
-                this.pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-
-                //Update encounter range to adventurer
-                if (this.adventurerEncounterRangeMarker != null) {
-                    this.adventurerEncounterRangeMarker.setCenter(this.pos);
-                }
-
-                console.log(this.pos.lat + ' ' + this.pos.lng);
-
-                //Updates Adventurer Marker
-                this.adventurerMarker.setPosition(this.pos);
-
-                //Monsters check for adventurer
-                // checkForAdventurer();
-                //Adventurer checks for monsters
-                // checkForMonster();
-                // checkForTreasure();
-                // checkForInteractable();
-            }, //end updateAdventurerSuccess
-
-            showHideEncounterRange () {
-                //First hide and clear old marker
-                if (this.adventurerEncounterRangeMarker != null) {
-                    this.adventurerEncounterRangeMarker.setMap(null);
-                    this.adventurerEncounterRangeMarker = null;
-                } else {
-                    //Build the Adventurer encounter radius here
-                    //This circle is the graphical representation of the range within whci adventurer will encounter monsters etc.
-                    this.adventurerEncounterRangeMarker = new google.maps.Circle({
-                        strokeColor: '#0000',
-                        strokeOpacity: 0.8,
-                        strokeWeight: 2,
-                        fillColor: 'blue',
-                        fillOpacity: 0.35,
-                        map: this.map,
-                        center: this.pos,
-                        radius: 400
-                    });
-                }
-            }, //end showHideEncounterRange
-        }, //end methods
+        methods: adventurerMethods,
 
         mounted() {
             this.initMap();
--- resources/assets/js/components/mapjs/adventurer.js
+++ resources/assets/js/components/mapjs/adventurer.js
@@ -1,107 +1,4 @@
-import mapIcon from './map-icons'; //import icons for map objects
 
-//watch postition vars
-let timeoutVal = 10 * 1000 * 1000;
-      
-let watchId = null;
-
-//Build the Adventurer Icon here
-let adventurerIcon = {
-          path: mapIcon['adventurer']['WingedSword'],
-          fillColor: 'white',
-          fillOpacity: 0.8,
-          scale: .1,
-          strokeColor: 'black',
-          strokeWeight: 1
-      };
-
-//Build the Adventurer encounter radius here
-//This circle is the graphical representation of the range within whci adventurer will encounter monsters etc.
-let adventurerEncounterRangeMarker = null;
-
-function generateAdventurer (pos, map ,marker) {
-  marker = new google.maps.Marker({
-          position: pos,
-          map: map,
-          icon: adventurerIcon,
-          title: 'Adventurer, you are here'
-        });
-
-  // showHideEncounterRangeMarker();
-}
-
-function updateAdventurerPosition (infoWindow, map, handleLocationError) {
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    
-    //Clear old watch first, then create new one
-    navigator.geolocation.clearWatch(watchId);
-    
-    //Set watch id and watch position
-     watchId = navigator.geolocation.watchPosition(successAdventurerWatch, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-      }, { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: (10 * 1000) }
-      );
-
-
-  
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-}
-
-function pauseUpdateAdventurerPosition () {
-  if (navigator.geolocation) {
-    navigator.geolocation.clearWatch(watchId);
-  }
-}
-
-function successAdventurerWatch(position) {
-      this.pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      //Update encounter range to adventurer
-      if (adventurerEncounterRangeMarker != null) {
-        adventurerEncounterRangeMarker.setCenter(this.pos);
-      }
-
-      console.log(this.pos);
-      
-      //Updates Adventurer Marker
-      this.adventurerMarker.setPosition(this.pos);
-
-      //Monsters check for adventurer
-      // checkForAdventurer();
-      //Adventurer checks for monsters
-      // checkForMonster();
-      // checkForTreasure();
-      // checkForInteractable();
-  }
-
-  function showHideEncounterRangeMarker () {
-    //First hide and clear old marker
-    if (adventurerEncounterRangeMarker != null) {
-      adventurerEncounterRangeMarker.setMap(null);
-      adventurerEncounterRangeMarker = null;
-    } else {
-
-      //Build the Adventurer encounter radius here
-      //This circle is the graphical representation of the range within whci adventurer will encounter monsters etc.
-      adventurerEncounterRangeMarker = new google.maps.Circle({
-          strokeColor: '#0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: 'blue',
-          fillOpacity: 0.35,
-          map: map,
-          center: pos,
-          radius: 400
-        });
-    }
-  }
 
 //Adventurer Checks for Monster
 //May later change this to checking for interactable
@@ -137,10 +34,135 @@ function checkForTreasure () {
     }
   }
 }
+import mapStyles from './map-styles'; //import map styles
+export default {
+            initMap() {
+                this.map = new google.maps.Map($('#map')[0], {
+                  center: {lat: 38.0423268, lng: -84.49276569999999},
+                  zoom: 14,
+                  styles: mapStyles['army']
+                });
+                this.infoWindow = new google.maps.InfoWindow;
 
+                this.getCurrentLocation();
+                this.updateAdventurerPosition(this.infoWindow, this.map, this.handleLocationError);
+            },
 
-export {generateAdventurer,
-        adventurerIcon,
-        updateAdventurerPosition,
-        successAdventurerWatch};
+            getCurrentLocation() {
+                // Try HTML5 geolocation.
+              if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position) => {
+
+                  this.pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                  };
+
+                  this.infoWindow.setPosition(this.pos);
+                  this.infoWindow.setContent('Adventurer, you are here');
+                  this.infoWindow.open(this.map);
+
+                  this.map.setCenter(this.pos);
+
+                  //Generates Adventurer Marker
+                  this.generateAdventurer(this.pos, this.map);
+
+                }, () => {
+                  this.handleLocationError(true, this.infoWindow, this.map.getCenter());
+                });
+              } else {
+                // Browser doesn't support Geolocation
+                this.handleLocationError(false, this.infoWindow, this.map.getCenter());
+              }
+            }, //end getCurrentLocation
+
+            handleLocationError(browserHasGeolocation, infoWindow, pos) {
+                infoWindow.setPosition(pos);
+                infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+                infoWindow.open(this.map);
+            }, //end handleLocationError
+
+            generateAdventurer () {
+                this.adventurerMarker = new google.maps.Marker({
+                      position: this.pos,
+                      map: this.map,
+                      icon: this.adventurerIcon,
+                      title: 'Adventurer, you are here'
+                    });
+
+                this.showHideEncounterRange();
+            }, //end generateAdventurer
+
+            updateAdventurerPosition (infoWindow, map, handleLocationError) {
+                // Try HTML5 geolocation.
+                if (navigator.geolocation) {
+
+                    //Clear old watch first, then create new one
+                    navigator.geolocation.clearWatch(this.watchId);
+
+                    //Set watch id and watch position
+                     this.watchId = navigator.geolocation.watchPosition(this.updateAdventurerSuccess, () => {
+                          this.handleLocationError(true, infoWindow, map.getCenter());
+                          }, { enableHighAccuracy: true, timeout: (10 * 1000 * 1000), maximumAge: (10 * 1000) }
+                          );
+
+                } else {
+                    // Browser doesn't support Geolocation
+                    this.handleLocationError(false, infoWindow, map.getCenter());
+                }
+            }, //end updateAdventurerPosition
+
+            pauseUpdateAdventurerPosition () {
+                if (navigator.geolocation) {
+                    navigator.geolocation.clearWatch(this.watchId);
+                }
+            }, //end updateAdventurerPositionStop
+
+            updateAdventurerSuccess (position) {
+                this.pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                //Update encounter range to adventurer
+                if (this.adventurerEncounterRangeMarker != null) {
+                    this.adventurerEncounterRangeMarker.setCenter(this.pos);
+                }
+
+                console.log(this.pos.lat + ' ' + this.pos.lng);
+
+                //Updates Adventurer Marker
+                this.adventurerMarker.setPosition(this.pos);
+
+                //Monsters check for adventurer
+                // checkForAdventurer();
+                //Adventurer checks for monsters
+                // checkForMonster();
+                // checkForTreasure();
+                // checkForInteractable();
+            }, //end updateAdventurerSuccess
+
+            showHideEncounterRange () {
+                //First hide and clear old marker
+                if (this.adventurerEncounterRangeMarker != null) {
+                    this.adventurerEncounterRangeMarker.setMap(null);
+                    this.adventurerEncounterRangeMarker = null;
+                } else {
+                    //Build the Adventurer encounter radius here
+                    //This circle is the graphical representation of the range within whci adventurer will encounter monsters etc.
+                    this.adventurerEncounterRangeMarker = new google.maps.Circle({
+                        strokeColor: '#0000',
+                        strokeOpacity: 0.8,
+                        strokeWeight: 2,
+                        fillColor: 'blue',
+                        fillOpacity: 0.35,
+                        map: this.map,
+                        center: this.pos,
+                        radius: 400
+                    });
+                }
+            }, //end showHideEncounterRange
+        } //end methods
 
