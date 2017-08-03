522bbd467f55c885d24bca7e1a6946da9bbb32f3
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-08-03 16:11:47 -0400

    landing page collapsible button fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,6 +80,12 @@
       crossorigin="anonymous"></script>
     <!-- Materialize -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/js/materialize.min.js"></script>
+    <script>
+        // Initialize Materialze Components
+    $(".button-collapse").sideNav(); //side navs
+
+    $(".dropdown-button").dropdown(); //dropdowns
+    </script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
