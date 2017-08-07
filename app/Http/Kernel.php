1bd0ff744b6e05a595b3d8c732b62f5d7db67d54
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-08-07 11:46:29 -0400

    migrate testing

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\HttpsProtocol::class,
+        // \App\Http\Middleware\HttpsProtocol::class,
        
     ];
 
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
              // appending custom middleware 
-            \App\Http\Middleware\HttpsProtocol::class,
+            // \App\Http\Middleware\HttpsProtocol::class,
         ],
 
         'api' => [
@@ -62,6 +62,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'https' => \App\Http\Middleware\HttpsProtocol::class,
+        // 'https' => \App\Http\Middleware\HttpsProtocol::class,
     ];
 }
